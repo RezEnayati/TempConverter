d2e78446c1cb46842f4097141db5b74f57dd014c
Reza Enayati <REZAREZA4302@GMAIL.COM>
2024-08-09 16:14:41 -0700

    Add files via upload

---
+++ TempConvertorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempConvertorApp.swift
+//  TempConvertor
+//
+//  Created by Reza Enayati on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct TempConvertorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
